9bad56ac70d1bbd254c9f8b8d680ade579550fd6
Gover912G <ngigipaul912@gmail.com>
2025-06-20 18:01:24 +0300

    links

--- food_mart/views.py
+++ food_mart/views.py
@@ -2,5 +2,14 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-   context = {'name': 'home'}
+   context = {
+      "name": "home"
+      }
    return render(request, 'index.html', context)
+
+
+def contact(request):
+   return render(request, 'contact.html', {'nav' : 'contact'})
+
+def about(request):
+   return render(request, 'about.html', {'nav' : 'about'})
