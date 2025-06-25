a25ac5f4e05a3685ec7ea21b60feb30c80f6324c
Gover912G <ngigipaul912@gmail.com>
2025-06-26 01:17:16 +0300

    urls link

--- food_mart/urls.py
+++ food_mart/urls.py
@@ -6,5 +6,17 @@ app_name = "foodmart"
 urlpatterns = [
     path('', views.home, name='home'),
     path('contact', views.contact, name = "contact"),
-    path('about', views.about, name = "about"),
+    path('about', views.about, name = 'about'),
+    path('shop', views.shop, name = "shop"),
+    path('single_product', views.singleProduct, name = "single-product"),
+    path('cart', views.cart, name = "cart"),
+    path('check_out', views.checkOut, name = "check-out"),
+    path('blog', views.blog, name = "blog"),
+    path('single_post', views.singlePost, name = "single-post"),
+    path('styles', views.styles, name = "styles"),
+    #path('thankyou', views.thankyou, name = "thankyou"),
+    path('account', views.myaccount, name = "account"),
+    #path('404', views.404, name = "404"),
+
+
 ]
--- food_mart/views.py
+++ food_mart/views.py
@@ -13,3 +13,28 @@ def contact(request):
 
 def about(request):
    return render(request, 'about.html', {'nav' : 'about'})
+
+def shop(request):
+   return render(request, 'shop.html', {'nav' : 'shop'})
+
+def singleProduct(request):
+   return render(request, 'single_product.html', {'nav' : 'single-post'})
+
+def cart(request):
+   return render(request, 'cart.html', {'nav':'cart'})
+
+def checkOut(request):
+   return render(request, 'check_out.html', {'nav' : 'check-out'})
+
+def blog(request):
+   return render(request, 'blog.html', {'nav' : 'blog'})
+
+def singlePost(request):
+   return render(request, 'single_post.html', {'nav' : "single-post"})
+
+def styles(request):
+   return render(request, 'styles.html', {'nav ' : 'styles'})
+
+def myaccount(request):
+   return render(request, 'accounts.html', {'nav' : 'account'})
+#thankyou
